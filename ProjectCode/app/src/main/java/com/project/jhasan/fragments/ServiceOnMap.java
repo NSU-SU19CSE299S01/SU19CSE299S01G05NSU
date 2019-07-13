675b271bd789b55d6307b6c45d98a58fcd8284a3
Md. Jahid Hasan Naiem <jhnaiem96@gmail.com>
2019-07-13 11:13:02 +0600

    Solving issue #22 : Basic layout and class file set up to show service on map

---
+++ ProjectCode/app/src/main/java/com/project/jhasan/fragments/ServiceOnMap.java
@@ -0,0 +1,38 @@
+package com.project.jhasan.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.project.jhasan.soudagor.R;
+
+
+public class ServiceOnMap extends Fragment {
+
+    MapView mapView;
+    GoogleMap map;
+
+    public ServiceOnMap() {
+        // Required empty public constructor
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_service_on_map, container, false);
+
+        return view;
+    }
+
+
+
+}
