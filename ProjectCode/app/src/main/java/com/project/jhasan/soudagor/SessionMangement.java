3909493989d81360e9f4942c6f471ab33d0f7e73
Md. Jahid Hasan Naiem 1620266642 <jahid.naiem@northsouth.edu>
2019-07-19 11:30:47 +0600

    Feature #23 : Add session related variables

--- ProjectCode/app/src/main/java/com/project/jhasan/soudagor/SessionMangement.java
+++ ProjectCode/app/src/main/java/com/project/jhasan/soudagor/SessionMangement.java
@@ -1,4 +1,35 @@
 package com.project.jhasan.soudagor;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SessionMangement {
+
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    SharedPreferences.Editor editor;
+
+    Context _context;
+
+
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "Service";
+
+
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+
+    public static final String KEY_NAME = "name";
+
+
+    public static final String KEY_EMAIL = "email";
+
+    public SessionMangement(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
 }
