cbf0060a21fd875701a17b04e075166455111109
Md. Jahid Hasan Naiem 1620266642 <jahid.naiem@northsouth.edu>
2019-07-19 11:41:04 +0600

    Feature #23 : Clear session details

--- ProjectCode/app/src/main/java/com/project/jhasan/soudagor/SessionMangement.java
+++ ProjectCode/app/src/main/java/com/project/jhasan/soudagor/SessionMangement.java
@@ -80,6 +80,23 @@ public class SessionMangement {
         return user;
     }
 
+    public void logoutUser(){
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+
+        // After logout redirect user to Loing Activity
+        Intent i = new Intent(_context, LoginActivity.class);
+        // Closing all the Activities
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Add new Flag to start new Activity
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Staring Login Activity
+        _context.startActivity(i);
+    }
+
     private boolean isLoggedIn() {
         return pref.getBoolean(IS_LOGIN, false);
     }
